4453089e0f6da842f1a1700ea468b3d459377e03
Fernando Julián Valdez <82127006+fervaldezjr@users.noreply.github.com>
2022-01-10 00:16:34 +0100

    Create dinamic title for list

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,6 +10,13 @@ const Form = ({ pacientes, setPacientes }) => {
 
     const [error, setError] = useState(false)
 
+    const generarId = () => {
+        const random = Math.random().toString(36).substr(2);
+        const fecha = Date.now().toString(36)
+
+        return random + fecha
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
@@ -29,7 +36,8 @@ const Form = ({ pacientes, setPacientes }) => {
             propietario, 
             email, 
             fecha, 
-            sintomas
+            sintomas,
+            id: generarId()
         }
 
         // console.log(objetoPaciente)
