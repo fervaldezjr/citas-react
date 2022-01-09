6c132ea49bf71b48d64317389a9f089586d17035
Fernando Julián Valdez <82127006+fervaldezjr@users.noreply.github.com>
2022-01-09 17:55:05 +0100

    Create dinamismo en las tarjetas de pacientes

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,15 +1,19 @@
-const Paciente = () => {
+const Paciente = ({paciente}) => {
+    
+    const { nombre, propietario, email, fecha, sintomas } = paciente
+
+    
     return (
         <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
-            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {""} <span className="font-normal normal-case">Hook</span></p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {""} <span className="font-normal normal-case">{nombre}</span></p>
             
-            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {""} <span className="font-normal normal-case">Fernando</span></p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {""} <span className="font-normal normal-case">{propietario}</span></p>
 
-            <p className="font-bold mb-3 text-gray-700 uppercase">Email: {""} <span className="font-normal normal-case">correo@correo.com</span></p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email: {""} <span className="font-normal normal-case">{email}</span></p>
 
-            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {""} <span className="font-normal normal-case">10 de Diciembre de 2020</span></p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {""} <span className="font-normal normal-case">{fecha}</span></p>
 
-            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {""} <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque a hic minima illo culpa id eos impedit, quaerat dolorum, ipsum amet, debitis nobis laborum! Aliquid dignissimos vitae sed quas doloremque!</span></p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {""} <span className="font-normal normal-case">{sintomas}</span></p>
         </div>
     )
 }
