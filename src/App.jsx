4cef44ebf87b2bf4304b7ac5294e2c8d37fdd4ae
Fernando Valdez <fervaldezjr11@gmail.com>
2022-01-19 10:59:53 +0100

    Fix paciente uundefined

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import { useState } from 'react';
-import Header from './components/Header';
-import Form from './components/Form';
-import ListadoPacientes from './components/ListadoPacientes';
+import { useState } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
-
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
 
   return (
     <div className="container mx-auto mt-3 p-8">
       <Header />
-      <div className='mt-12 md:flex'>
+      <div className="mt-12 md:flex">
         <Form 
-        pacientes = { pacientes }
-        setPacientes = { setPacientes }
+          pacientes={pacientes} 
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
         />
-        <ListadoPacientes 
-          pacientes = { pacientes }
-          setPaciente = { setPaciente }
-          paciente = {paciente}
+        <ListadoPacientes
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          paciente={paciente}
         />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,8 +10,14 @@ const Form = ({ pacientes, setPacientes, paciente }) => {
 
     const [error, setError] = useState(false);
 
-    useEffect( () => {
-
+    useEffect(() => {
+        if( Object.keys(paciente).length > 0  ) {
+            setNombre(paciente.nombre)
+            setPropietario(paciente.propietario)
+            setEmail(paciente.email)
+            setFecha(paciente.fecha)
+            setSintomas(paciente.sintomas)
+        }
     }, [paciente])
 
     const generarId = () => {
